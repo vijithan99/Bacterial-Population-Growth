9ea73471e359d23bc33f5ed433118c21e4d730fb
vijithan99 <47492407+vijithan99@users.noreply.github.com>
2019-09-24 12:50:34 -0400

    Add files via upload

---
+++ Lab02.h
@@ -0,0 +1,18 @@
+/*
+ * Lab02.h
+ *
+ *  Created on: Sep 23, 2019
+ *      Author: vmangale
+ */
+
+#ifndef LAB02_H_
+#define LAB02_H_
+
+double BacteriaKAsk () ;
+double BacteriaNAsk () ;
+void PopulationCalc ();
+void PopulationPrint ();
+
+
+
+#endif /* LAB02_H_ */
